759d1e07e82195b428a10c5d519871d690422a34
Mbedzi346 <orifha@mbedzi.co.za>
2019-07-03 23:02:22 +0200

    Added permanent storage using LevelDB

--- simpleChain.js
+++ simpleChain.js
@@ -1,7 +1,6 @@
 const SHA256 = require('crypto-js/sha256');
 var level = require('level') //persistent data store
-
-var db = level('simpleBlockChain')
+var db = level('simpleBlockdb', {valueEncoding : 'json'});
 class Block {
   constructor(data){
     this.hash = "",
@@ -23,17 +22,67 @@ class Blockchain{
       newBlock.previousBlockHash = this.chain[this.chain.length - 1].hash
     }
     newBlock.hash = SHA256(JSON.stringify(newBlock)).toString();
-    this.chain.push(newBlock)
-    db.put('name', newBlock, function(err){
-      if(err)
-        return console.log(err)
-    })
+    this.chain.push(newBlock);
+    db.put(newBlock.height, {
+      "hash" : newBlock.hash,
+      "height" : newBlock.height,
+      "body" : [newBlock.body],
+      "time" : newBlock.time,
+      "previousBlockHash" : newBlock.previousBlockHash
+    }, function (err) {
+            if (!err)
+                console.log("Block successfully added.")
+        }
+    )
+
+
   }
+  getBlockHeight(){
+    return this.chain.length - 1;
+  }
+  getBlock(blockHeight){
+    return this.chain[blockHeight-1];
+  }
+    validateBlock(blockHeight){
+        let block = this.getBlock(blockHeight);
+        let blockHash = block.hash;
+        block.hash = '';
+        let validBlockHash = SHA256(JSON.stringify(block)).toString();
+        if (blockHash===validBlockHash) {
+            return true;
+        } else {
+            console.log('Block #'+blockHeight+' invalid hash:\n'+blockHash+'<>'+validBlockHash);
+            return false;
+        }
+    }
+    validateChain(){
+        let errorLog = [];
+        for (var i = 0; i < this.chain.length-1; i++) {
+            // validate block
+            if (!this.validateBlock(i))errorLog.push(i);
+            // compare blocks hash link
+            let blockHash = this.chain[i].hash;
+            let previousHash = this.chain[i+1].previousBlockHash;
+            if (blockHash!==previousHash) {
+                errorLog.push(i);
+            }
+        }
+        if (errorLog.length>0) {
+            console.log('Block errors = ' + errorLog.length);
+            console.log('Blocks: '+errorLog);
+        } else {
+            console.log('No errors detected');
+        }
+    }
+    getBlockByHeight(height){
+        db.get(height, function (err, block) {
+            console.log(block)
+        })
+    }
 }
 //Test
 blockchain = new Blockchain();
 blockchain.addBlock(new Block("First block"))
 blockchain.addBlock(new Block("Second block"))
 blockchain.addBlock(new Block("Third block"))
-console.log(db.get('a385849a556839f08e797a9bc22f2bb4baa5bf89994369917bc1b4eb9bb79d52'))
-//console.log(blockchain.chain)
+console.log(blockchain.getBlockByHeight(2))
